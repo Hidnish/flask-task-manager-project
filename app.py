f2ab0bfdb8ba3ae0bfb74ec659242e4633e250e5
Hidnish <hideonishimura@live.it>
2021-08-12 14:32:58 +0000

    Create base template

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app) # Constructor --> the "app" is the variable created above 
 
+
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
-    
 
 
 if __name__ == "__main__":
