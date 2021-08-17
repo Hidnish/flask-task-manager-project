baa23a0c80573a46ebff50b31d28dc8491505fa9
Hidnish <hideonishimura@live.it>
2021-08-17 15:52:10 +0000

    Add register functionality

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_pymongo import PyMongo  # different from when you insall it from  
-                                   # command line (flask-pymongo, pymongo)
-from bson.objectid import ObjectId  # render object id in order to find documents from mongoDB later 
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
@@ -16,7 +15,7 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app) # Constructor --> the "app" is the variable created above 
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -28,10 +27,28 @@ def get_tasks():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=os.environ.get("PORT"),
+            port=int(os.environ.get("PORT")),
             debug=True)
