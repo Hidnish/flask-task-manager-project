024fc36d67975d2658ca4814d0272e0439b6c914
Hidnish <hideonishimura@live.it>
2021-08-27 16:03:28 +0000

    Update task into the database

--- app.py
+++ app.py
@@ -122,8 +122,23 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
-    task = mongo.db.tasks.find_one({ "_id": ObjectId(task_id)})
 
+    if request.method == "POST":
+        # python uses the "name" attribute from html to create key-value pair
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            # you could use .getlist if you want to store an array, such as a list of ingredients
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash("Task Successfully Updated!")
+
+    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
 
